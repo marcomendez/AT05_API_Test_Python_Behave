1a41e9d40eb28c55f102fab95091b0a710c16da0
marco mendez <marco.mendez@jalasoft.com>
2019-04-04 14:15:54 -0400

    fiall

--- MarcoMendez/Practices/ClassPractice/Workers/Employee.py
+++ MarcoMendez/Practices/ClassPractice/Workers/Employee.py
@@ -7,6 +7,20 @@ class Employee(Person):
         Person.__init__(self,name, lastName,age,ci)
         self.EmployeeId = employeeId
         self.Departmet = department
+asdada
+
+
+asd
+a
+da
+sd
+a
+sd
+a
+da
+d
+a
+sdas
 
     def returInfo(self):
         return self.Name+ ", "+ self.LastName + ", "+  self.Age + ", " + self.CI + ", "+ self.EmployeeId + ", "+  self.Departmet
